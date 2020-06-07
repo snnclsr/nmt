d8d7d4f7462b911a48c19d84363943be9e66883c
Sinan <sinancalisir001@gmail.com>
2020-06-08 02:36:42 +0300

    aaaand working example

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from datetime import datetime
+import pickle
 
-from config import HOST, PORT, DEBUG
+from config import HOST, PORT, DEBUG, MODEL_PATH
 from flask import Flask, render_template, request
+from utils import to_tensor
+from models import Seq2Seq
+from test import beam_search
+from vocab import Vocab
+from train import Vocabularies
+
+from nltk.tokenize import RegexpTokenizer
+
+puncts_except_apostrophe = '!"#$%&\()*+,-./:;<=>?@[\\]^_`{|}~'
+TOKENIZE_PATTERN = fr"[{puncts_except_apostrophe}]|\w+|['\w]+"
+tokenizer = RegexpTokenizer(pattern=TOKENIZE_PATTERN)
 
 app = Flask(__name__)
 
@@ -21,14 +32,32 @@ def translate():
 
     print(args)
     text_input = args["textarea"]
-    print("Input: ", text_input)
 
-    result_hypothesis = [
-        "123",
-        "345",
-        "678"
-    ]
-    return render_template("index.html", hypothesis=result_hypothesis)
+    print("Input: ", text_input)
+    tokenized_sent = tokenizer.tokenize(text_input)
+    print("Tokenized input: ", tokenized_sent)
+
+    with open("vocabs.pkl", "rb") as f:
+        vocabs = pickle.load(f)
+
+    model = Seq2Seq.load(MODEL_PATH)
+    model.device = "cpu"
+    hypothesis = beam_search(model, [tokenized_sent], beam_size=3, max_decoding_time_step=70)
+
+    hypothesis = hypothesis[0]
+    result_hypothesis = []
+    for hyp in hypothesis:
+        result_hypothesis.append(" ".join(hyp[0]))
+
+    print(result_hypothesis)
+
+    # print(hypothesis)
+    # result_hypothesis = [
+    #     "123",
+    #     "345",
+    #     "678"
+    # ]
+    return render_template("index.html", hypothesis=result_hypothesis, sentence=text_input)
 
 
 
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 HOST = "0.0.0.0"
 PORT = 8080
 DEBUG = True
+MODEL_PATH = "model.bin"
